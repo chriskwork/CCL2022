90bf34ddad1df83189fa1436c0d3819324ac3ded
chris k <yovirinc@gmail.com>
2022-07-06 01:37:01 +0200

    tema voca page working..

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,12 +1,7 @@
 <!-- Page Template -->
 
-<div class="page-content page-padding" style="height: 100vh;">
-  <div class="page-header">
-    <h1 class="page-header-title">
-      <?php the_title(); ?>
-    </h1>
-    <!-- <div class="page-header-img"></div> -->
-  </div>
+<div class="page-content page-padding">
+  <!-- <div class="page-header"></div> -->
 
   <div class="container">
     <div class="page-body">
