c400d39706147eec862864504e9eca0e98e54253
chris k <yovirinc@gmail.com>
2024-01-13 12:08:29 +0100

    added goToTop button and some minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,29 @@ switchColorMode(+savedMode);
 // + 표시는 string을 number로 변환해준다.
 
 //
+// ############ Go To Top Button
+//
+// Get the button
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
